80062f5fdcb086ce13f0f39d72baec2df5b70bf6
herp2021 <erphimalaya@gmail.com>
2025-09-08 15:03:39 +0530

    Update clinics.js

--- src/data/clinics.js
+++ src/data/clinics.js
@@ -1,1 +1,20 @@
+// Data for the physiotherapy clinics.
+const clinics = [
+    {
+        name: "Best Physiotherapy Clinic/সেরা ফিজিওথেরাপি ক্লিনিক",
+        address: "Thakurpukur - Bibirhat - Bakhrahat - Raipur Rd, Bakhrahat, Raypur, West Bengal 743377, India",
+        openingHours: "Monday - Saturday: 4:00 PM - 8:00 PM",
+        mapUrl: "https://maps.google.com/?cid=2164312957080943272",
+        phone: null,
+    },
+    {
+        name: "Arogya Physiotherapy clinic/আরোগ্য ফিজিওথেরাপি ক্লিনিক",
+        address: "Pundooah Station Rd, Pandua, West Bengal 712149, India",
+        openingHours: "Monday - Saturday: 8:00 AM - 8:00 PM",
+        mapUrl: "https://maps.google.com/?cid=8026987565069353680",
+        phone: "+91 77977 43478",
+    },
+];
+
+export default clinics;
 
