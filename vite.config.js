1ff2f81346a416b8fa92d1f72456333ecac3edfc
herp2021 <erphimalaya@gmail.com>
2025-09-08 16:08:05 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: '.',
+  root: 'public', // Corrected root to point to the 'public' directory
   build: {
     outDir: 'dist',
   },
