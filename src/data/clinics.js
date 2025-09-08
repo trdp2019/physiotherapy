f13c9d09f4656f48f8408277cf28a3a434ee349d
herp2021 <erphimalaya@gmail.com>
2025-09-08 15:04:04 +0530

    Update clinics.js

--- src/data/clinics.js
+++ src/data/clinics.js
@@ -5,7 +5,7 @@ const clinics = [
         address: "Thakurpukur - Bibirhat - Bakhrahat - Raipur Rd, Bakhrahat, Raypur, West Bengal 743377, India",
         openingHours: "Monday - Saturday: 4:00 PM - 8:00 PM",
         mapUrl: "https://maps.google.com/?cid=2164312957080943272",
-        phone: null,
+        phone: "+91 77977 43478",
     },
     {
         name: "Arogya Physiotherapy clinic/আরোগ্য ফিজিওথেরাপি ক্লিনিক",
