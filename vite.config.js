9b4bcd99d37a13596c9d48a3c34d52f74f6652dc
Tridip Dutta <47116740+trdp2019@users.noreply.github.com>
2025-09-08 16:39:16 +0530

    Update vite.config.js for removing public root!

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: 'public', // Corrected root to point to the 'public' directory
   build: {
     outDir: 'dist',
   },
