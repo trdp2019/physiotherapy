212c0e000ab905faa1e3e5d0a8b2529338c35d93
herp2021 <erphimalaya@gmail.com>
2025-09-08 15:08:13 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './styles/tailwind.css';
+
+// Find the root element in the HTML.
+const rootElement = document.getElementById('root');
+
+// Create a React root and render the App component.
+const root = ReactDOM.createRoot(rootElement);
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
 
