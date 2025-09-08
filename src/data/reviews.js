76ef6835fa944e3f15b6348d43802157d0730dfd
herp2021 <erphimalaya@gmail.com>
2025-09-08 15:04:44 +0530

    Update reviews.js

--- src/data/reviews.js
+++ src/data/reviews.js
@@ -1,1 +1,21 @@
+// Dummy data for Google reviews.
+const reviews = [
+    {
+        author: "Rahul Sharma",
+        rating: 5,
+        comment: "The clinic provides excellent care. The staff is knowledgeable and compassionate. My back pain has significantly improved!",
+    },
+    {
+        author: "Priya Singh",
+        rating: 5,
+        comment: "A wonderful experience! The therapist took the time to understand my issue and created a personalized plan. Highly recommended.",
+    },
+    {
+        author: "Ankit Kumar",
+        rating: 4,
+        comment: "Great service and clean facility. The therapy sessions were very effective. I am very satisfied with the results.",
+    },
+];
+
+export default reviews;
 
